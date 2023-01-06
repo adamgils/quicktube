49c3753f31a17f26aa9d5cc72dc4d0022c98ecf5
adamgils <121285724+adamgils@users.noreply.github.com>
2023-01-06 14:48:54 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -12,9 +12,10 @@ st.markdown("Welcome to QuickTube, the fastest way to summarize videos on YouTub
 video_url = st.text_input("_:red[Enter a YouTube URL:]_")
 st.markdown("_Created by [Adam Gilani](https://twitter.com/adamgilani)_")
 
-# Feeds API Key From StreamLit
+# Feeds API Key From StreamLit "Secrets"
 openai.api_key = st.secrets["API_KEY"]
 
+# Extracts YouTube Video ID from URL
 def extract_video_ID():
     if 'watch' in video_url:
         return video_url.split("?v=")[-1]
@@ -33,27 +34,27 @@ def summarize():
     transcript_text = " ".join([line["text"] for line in transcript])
 
     # Divide the transcript into chunks of 3500 characters
-    chunk_size = 1500
+    chunk_size = 3500
     num_chunks = math.ceil(len(transcript_text) / chunk_size)
     chunks = [transcript_text[i * chunk_size:(i + 1) * chunk_size] for i in range(num_chunks)]
 
     # Initialize an empty list to store the summaries
     summaries = []
 
-    # Send each chunk to the OpenAI Completion API to generate a summary
+    # Send each chunk to the OpenAI GPT-3 API to generate a summary
     for chunk in chunks:
         prompt = f"Summarize into a concise but detailed paragraph, leave out anything that is not relevant: {chunk}"
         estimated_tokens = len(prompt) / 4
 
-        # Send the prompt to the Completion API
+        # Send the prompt to the GPT-3 API
         response = openai.Completion.create(
             engine="text-curie-001",
             prompt=prompt,
             temperature=1,
-            max_tokens=int(1800 - estimated_tokens)
+            max_tokens=int(3800 - estimated_tokens)
         )
 
-        # Add the summary to the list
+        # Add each summary to the list
         summaries.append(response["choices"][0]["text"])
 
     # Concatenate the summaries into a single string
@@ -61,10 +62,10 @@ def summarize():
     return summary
 
 if video_url:
-    # Display the spinner
+    # Text generation spinner
     with st.spinner("Please wait while your text is being generated..."):
         # Generate the summarization text
         summary = summarize()
 
-    # Write the summarization text to the app
+    # Feed the summarization text to the app
     st.write(summary)
